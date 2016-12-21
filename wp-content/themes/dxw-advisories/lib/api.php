0beb3f2ffe2747624c30c1e571883a15927d214b
Duncan Stuart <dgmstuart@gmail.com>
2016-12-21 12:54:19 +0000

    (refactor) extract a Router
    This refactoring is probably premature at this point. The point is to
    separate out the building of the Controller class from the creation of
    the API endpoints.
    
    I don't feel like there's anything here which can be unit tested: it's
    all configuration and implementation detail, so any test would break if
    the implementation changed.
    
    The only part which seems amenable to testing is the regex, but I can't
    see how this could be tested without exercising the real
    `register_rest_route` function.

--- wp-content/themes/dxw-advisories/lib/api.php
+++ wp-content/themes/dxw-advisories/lib/api.php
@@ -1,11 +1,10 @@
 <?php
 add_action('rest_api_init', function () {
-    $slug_regex = '[a-z0-9]+(?:-[a-z0-9]+)*';
     $finder = new \DxwSec\API\InspectionsFinder();
     $json_finder = new \DxwSec\API\JSONInspectionsFinder($finder);
-    $controller = new \DxwSec\API\InspectionsController($json_finder);
-    register_rest_route('v1', 'inspections/(?P<slug>'.$slug_regex.')', [
-        'methods' => 'GET',
-        'callback' => [$controller, 'show'],
-    ]);
+
+    $inspections_controller = new \DxwSec\API\InspectionsController($json_finder);
+
+    $router = new \DxwSec\API\Router($inspections_controller);
+    $router->registerRoutes();
 });
