b0b92a1d8192ed2d024e8a0a2776ec3025ff089e
Duncan Stuart <dgmstuart@gmail.com>
2017-05-19 12:56:34 +0100

    (fix) Hide failure criteria when there are none
    The logic previously was:
    
      - Show the whole section if there were any failure criteria
    
    That implied that in order for there to be a reason there HAD to be some
    failure criteria checked, which is not the case e.g. if the review is
    for some unusual reason, or simply at the reviewer's discretion.
    
    The logic now is:
    
      - Display the section if the review was yellow or red
      - Display the list of selected failure criteria only if there are some
        checked

--- wp-content/themes/dxw-advisories/templates/content-plugin.php
+++ wp-content/themes/dxw-advisories/templates/content-plugin.php
@@ -45,31 +45,29 @@
       <h3>Findings</h3>
       <p><?php echo get_field('findings') ?></p>
       <?php
-        $failure_criteria = get_field_label('matched_criteria');
-        if(is_array($failure_criteria) && count($failure_criteria)) {
+        $recommendation = get_field('recommendation');
+        if($recommendation != 'green') {
       ?>
-
         <h3>Failure criteria</h3>
+        <p><?php echo get_field_label('recommendation_criterion_' . $recommendation) ?>:</p>
+        <blockquote><p><?php echo get_field('reason') ?></p></blockquote>
+
         <?php
-          $recommendation = get_field('recommendation');
-
-          if($recommendation != 'green') {
-            echo '<p>' . get_field_label('recommendation_criterion_' . $recommendation) . ':</p>';
-            echo '<blockquote><p>' . get_field('reason') . '</p></blockquote>';
-          }
+          $failure_criteria = get_field_label('matched_criteria');
+          if(is_array($failure_criteria) && count($failure_criteria)) {
         ?>
-
-        <p>Read more about our <a href="/about/plugin-inspections/#failure_criteria">failure criteria</a>.</p>
-        <table class="failure_criteria table-striped table">
-        <tbody>
-          <?php foreach($failure_criteria as $criterion) { ?>
-            <tr>
-              <td class="span1 bad"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/bad.png" width="22" height="22" alt="Fail"></td>
-              <td><?php echo $criterion ?></td>
-            </tr>
-          <?php } ?>
-        </tbody>
-        </table>
+          <p>Read more about our <a href="/about/plugin-inspections/#failure_criteria">failure criteria</a>.</p>
+          <table class="failure_criteria table-striped table">
+          <tbody>
+            <?php foreach($failure_criteria as $criterion) { ?>
+              <tr>
+                <td class="span1 bad"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/bad.png" width="22" height="22" alt="Fail"></td>
+                <td><?php echo $criterion ?></td>
+              </tr>
+            <?php } ?>
+          </tbody>
+          </table>
+        <?php } ?>
       <?php } ?>
     </div>
 
