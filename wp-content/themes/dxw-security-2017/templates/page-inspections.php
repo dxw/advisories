2f5dbe8539cb7c9f27b14298d82b308f8ac8af01
Magda Faizov <magda@dxw.com>
2017-08-03 17:00:43 +0100

    (feature) card 50, Inspection page ACFs, Inspections page template

--- wp-content/themes/dxw-security-2017/templates/page-inspections.php
+++ wp-content/themes/dxw-security-2017/templates/page-inspections.php
@@ -6,9 +6,38 @@
 
 <div class="row">
     <div class="page-container">
-        <article class="rich-text">
-            <?php the_post();
-            the_content(); ?>
-        </article>
+        <div class="page-content">
+            <article class="rich-text">
+                <?php the_post();
+                the_content(); ?>
+            </article>
+            <?php get_template_part('partials/recommendations'); ?>
+            <?php if (get_field('failure_criteria_description')) : ?>
+                <section class="failure-criteria">
+                    <h2>Failure criteria</h2>
+                    <?php the_field('failure_criteria_description'); ?>
+                    <table class="criteria-table">
+                        <caption><?php the_field('table_caption'); ?></caption>
+                        <thead>
+                            <tr>
+                                <th scope="col">Criterion</th>
+                                <th scope="col">Explanation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if (have_rows('failure_criteria_table')) :
+                                while (have_rows('failure_criteria_table')) :
+                                    the_row(); ?>
+                                    <tr>
+                                        <td><?php the_sub_field('criterion'); ?></td>
+                                        <td><?php the_sub_field('explanation'); ?></td>
+                                    </tr>
+                                <?php endwhile;
+                            endif; ?>
+                        </tbody>
+                    </table>
+                </section>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
---
+++ wp-content/themes/dxw-security-2017/templates/partials/recommendations.php
@@ -0,0 +1,27 @@
+<?php if (get_field('recommendations_heading')) : ?>
+    <section class="recommendations">
+        <h2><?php the_field('recommendations_heading'); ?></h2>
+        <?php if (have_rows('recommendations')) :
+            while (have_rows('recommendations')) :
+                the_row();
+                $security = get_sub_field('security_warning');
+                $subtitle = get_sub_field('subtitle'); ?>
+                <article>
+                    <h3 class="<?php echo $security; ?>">
+                        <?php if ($security == 'red') :
+                            echo 'Potentially unsafe';
+                        elseif ($security == 'orange') :
+                            echo 'Use with caution';
+                        elseif ($security == 'green') :
+                            echo 'No issues found';
+                        endif; ?>
+                    </h3>
+                    <div class="rich-text">
+                        <p><strong><?php echo $subtitle; ?></strong></p>
+                        <?php the_sub_field('description'); ?>
+                    </div>
+                </article>
+            <?php endwhile;
+        endif; ?>
+    </section>
+<?php endif; ?>
