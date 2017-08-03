979f7cddd0e3fc7cc3977f31b513d1e9bc2b01b1
Magda Faizov <magda@dxw.com>
2017-08-03 09:58:12 +0100

    (feature) card 39, removed not used values from global search, added screen reader text and fixed features boxes width on homepage

--- wp-content/themes/dxw-security-2017/templates/partials/global-search-form.php
+++ wp-content/themes/dxw-security-2017/templates/partials/global-search-form.php
@@ -3,8 +3,8 @@
         <form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url('/') ?>">
             <label for="s" class="block-label">Search plugins &amp; advisories:</label>
             <div class="form-field">
-                <input type="search" value="<?php echo get_search_query() ?>" name="s" id="s-<?php echo esc_html($type) ?>">
-                <button value="Search <?php echo esc_html($type) ?>" class="button">Search</button>
+                <input type="search" value="<?php echo get_search_query() ?>" name="s">
+                <button value="Search" class="button">Search</button>
             </div>
         </form>
     </div>
--- wp-content/themes/dxw-security-2017/templates/search.php
+++ wp-content/themes/dxw-security-2017/templates/search.php
@@ -12,7 +12,7 @@
             <?php the_excerpt() ?>
         </div>
         <footer>
-            <a class="read-more" href="<?php the_permalink() ?>">Read more</a>
+            <a class="read-more" href="<?php the_permalink() ?>">Read more <span class="sr-only">about this topic</span></a>
             —
             <a href="<?php comments_link() ?>"><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number())) ?></a>
         </footer>
