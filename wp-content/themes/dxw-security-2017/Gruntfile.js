6671d3ea84bf1f24e42468242d517a2a07accfc3
Rob Skilling <rob@dxw.com>
2020-05-21 11:01:10 +0100

    Migrate from npm to Yarn, remove bower
    I've had to upgrade grunt-sass in order to do this, as the old version would cause an error when trying to install. That means I've also had to add node-sass, as you have to provide a sass implementation option for newer versions of grunt-sass.
    
    We don't use bower anymore, as it's simpler to just do everything in yarn. So I've also got rid of the bower things and loaded jQuery via yarn as well.

--- wp-content/themes/dxw-security-2017/Gruntfile.js
+++ wp-content/themes/dxw-security-2017/Gruntfile.js
@@ -12,6 +12,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean')
   grunt.loadNpmTasks('grunt-svgmin')
 
+  const sass = require('node-sass')
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -25,6 +27,7 @@ module.exports = function (grunt) {
 
     sass: {
       options: {
+        implementation: sass,
         outputStyle: 'compressed',
         sourceMap: true,
         includePaths: [
@@ -82,7 +85,7 @@ module.exports = function (grunt) {
     copy: {
       production: {
         files: {
-          'static/lib/jquery.min.js': 'bower_components/jquery/dist/jquery.min.js'
+          'static/lib/jquery.min.js': 'node_modules/jquery/dist/jquery.min.js'
         }
       }
     },
@@ -129,15 +132,6 @@ module.exports = function (grunt) {
     }
   })
 
-  grunt.registerTask('bower-install', 'Installs bower deps', function () {
-    var done = this.async()
-    var bower = require('bower')
-
-    bower.commands.install().on('end', function () {
-      done()
-    })
-  })
-
   // Hack to make `img` task work
   grunt.registerTask('img-mkdir', 'mkdir static/img', function () {
     var fs = require('fs')
@@ -155,7 +149,6 @@ module.exports = function (grunt) {
 
   grunt.registerTask('default', [
     'clean',
-    'bower-install',
     'img-mkdir',
     'img',
     'svgmin',
