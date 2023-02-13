c03126f5587feb68ac2bc3905fb7a6e12ac4baf9
Sarah Mount <sarah.mount@dxw.com>
2023-02-13 19:11:38 +0000

    Convert spaces to tabs in all scripts

--- setup/internal.sh
+++ setup/internal.sh
@@ -18,49 +18,49 @@ wp core install --skip-email --admin_user=admin --admin_password=admin --admin_e
 # Plugins
 for plugin in $plugins
 do
-  if wp plugin is-installed "$plugin"
-  then
-    wp plugin activate "$plugin"
-  else
-    # shellcheck disable=SC2028,SC2027,SC2086
-    echo "\033[96mWarning:\033[0m Plugin '"$plugin"' could not be found. Have you installed it?"
-  fi
+	if wp plugin is-installed "$plugin"
+	then
+		wp plugin activate "$plugin"
+	else
+		# shellcheck disable=SC2028,SC2027,SC2086
+		echo "\033[96mWarning:\033[0m Plugin '"$plugin"' could not be found. Have you installed it?"
+	fi
 done
 
 # Theme
 if wp theme is-installed "$theme"
 then
 
-  wp theme activate "$theme"
+	wp theme activate "$theme"
 else
-  # shellcheck disable=SC2028,SC2027,SC2086
-  echo "\033[96mWarning:\033[0m Theme '"$theme"' could not be found. Have you installed it?"
+	# shellcheck disable=SC2028,SC2027,SC2086
+	echo "\033[96mWarning:\033[0m Theme '"$theme"' could not be found. Have you installed it?"
 fi
 
 # Content
 if [ "$(ls -A $content)" ]
 then
-  if wp plugin is-installed wordpress-importer
-  then
-    wp plugin activate wordpress-importer
-    for file in "$content"/*.xml
-    do
-      echo "Importing $file..."
-      wp import "$file" --authors=skip
-    done
-  else
-    echo "WordPress Importer not installed... installing now"
-    wp plugin install wordpress-importer --activate
-    for file in "$content"/*.xml
-    do
-      echo "Importing $file..."
-      wp import "$file" --authors=skip
-    done
-    wp plugin uninstall wordpress-importer --deactivate
-  fi
+	if wp plugin is-installed wordpress-importer
+	then
+		wp plugin activate wordpress-importer
+		for file in "$content"/*.xml
+		do
+			echo "Importing $file..."
+			wp import "$file" --authors=skip
+		done
+	else
+		echo "WordPress Importer not installed... installing now"
+		wp plugin install wordpress-importer --activate
+		for file in "$content"/*.xml
+		do
+			echo "Importing $file..."
+			wp import "$file" --authors=skip
+		done
+		wp plugin uninstall wordpress-importer --deactivate
+	fi
 
 else
-  echo "No content to be imported"
+	echo "No content to be imported"
 fi
 
 # Other setup
