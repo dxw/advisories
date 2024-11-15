f45b7d863afa336085d486c6c3a51d33a462257d
Serena <serena@dxw.com>
2024-11-15 12:00:43 +0000

    Fix fatal error on Options page

--- wp-content/themes/dxw-security-2017/app/Theme/OptionsPage.php
+++ wp-content/themes/dxw-security-2017/app/Theme/OptionsPage.php
@@ -6,103 +6,107 @@ class OptionsPage implements \Dxw\Iguana\Registerable
 {
 	public function register()
 	{
-		acf_add_options_sub_page('Banner');
+		if (function_exists('acf_add_options_sub_page')) {
+			acf_add_options_sub_page('Banner');
+		}
 
 		// Hire us banner
-		acf_add_local_field_group([
-			'key' => 'group_5857b450d2170',
-			'title' => 'Call to action banner',
-			'fields' => [
-				[
-					'key' => 'field_5857b463ac3e9',
-					'label' => 'Header',
-					'name' => 'header',
-					'type' => 'text',
-					'instructions' => '',
-					'required' => 0,
-					'conditional_logic' => 0,
-					'wrapper' => [
-						'width' => '',
-						'class' => '',
-						'id' => '',
-					],
-					'default_value' => '',
-					'placeholder' => '',
-					'prepend' => '',
-					'append' => '',
-					'maxlength' => '',
-				],
-				[
-					'key' => 'field_5857b46dac3ea',
-					'label' => 'Content',
-					'name' => 'content',
-					'type' => 'wysiwyg',
-					'instructions' => '',
-					'required' => 0,
-					'conditional_logic' => 0,
-					'wrapper' => [
-						'width' => '',
-						'class' => '',
-						'id' => '',
-					],
-					'default_value' => '',
-					'tabs' => 'all',
-					'toolbar' => 'basic',
-					'media_upload' => 0,
-				],
-				[
-					'key' => 'field_5857b47dac3eb',
-					'label' => 'URL',
-					'name' => 'url',
-					'type' => 'url',
-					'instructions' => '',
-					'required' => 0,
-					'conditional_logic' => 0,
-					'wrapper' => [
-						'width' => '',
-						'class' => '',
-						'id' => '',
-					],
-					'default_value' => '',
-					'placeholder' => '',
-				],
-				[
-					'key' => 'field_5857b491ac3ec',
-					'label' => 'Call to action',
-					'name' => 'cta',
-					'type' => 'text',
-					'instructions' => '',
-					'required' => 0,
-					'conditional_logic' => 0,
-					'wrapper' => [
-						'width' => '',
-						'class' => '',
-						'id' => '',
-					],
-					'default_value' => '',
-					'placeholder' => '',
-					'prepend' => '',
-					'append' => '',
-					'maxlength' => '',
-				],
-			],
-			'location' => [
-				[
+		if (function_exists('acf_add_local_field_group')) {
+			acf_add_local_field_group([
+				'key'                   => 'group_5857b450d2170',
+				'title'                 => 'Call to action banner',
+				'fields'                => [
 					[
-						'param' => 'options_page',
-						'operator' => '==',
-						'value' => 'acf-options-banner',
+						'key'               => 'field_5857b463ac3e9',
+						'label'             => 'Header',
+						'name'              => 'header',
+						'type'              => 'text',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'placeholder'       => '',
+						'prepend'           => '',
+						'append'            => '',
+						'maxlength'         => '',
+					],
+					[
+						'key'               => 'field_5857b46dac3ea',
+						'label'             => 'Content',
+						'name'              => 'content',
+						'type'              => 'wysiwyg',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'tabs'              => 'all',
+						'toolbar'           => 'basic',
+						'media_upload'      => 0,
+					],
+					[
+						'key'               => 'field_5857b47dac3eb',
+						'label'             => 'URL',
+						'name'              => 'url',
+						'type'              => 'url',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'placeholder'       => '',
+					],
+					[
+						'key'               => 'field_5857b491ac3ec',
+						'label'             => 'Call to action',
+						'name'              => 'cta',
+						'type'              => 'text',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'placeholder'       => '',
+						'prepend'           => '',
+						'append'            => '',
+						'maxlength'         => '',
 					],
 				],
-			],
-			'menu_order' => 0,
-			'position' => 'normal',
-			'style' => 'default',
-			'label_placement' => 'top',
-			'instruction_placement' => 'label',
-			'hide_on_screen' => '',
-			'active' => 1,
-			'description' => '',
-		]);
+				'location'              => [
+					[
+						[
+							'param'    => 'options_page',
+							'operator' => '==',
+							'value'    => 'acf-options-banner',
+						],
+					],
+				],
+				'menu_order'            => 0,
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => 1,
+				'description'           => '',
+			]);
+		}
 	}
 }
