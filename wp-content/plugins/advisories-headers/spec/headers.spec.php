e87c8ac72845ceb8421f8decbf84e7c471817f68
Sarah Mount <sarah.mount@dxw.com>
2025-06-23 18:18:40 +0100

    Add Cache-Control headers to all pages
    * If the user is logged in we do not cache
    * If the page is a homepage or archive page or the
      request is  search query, we use a short cache lifetime
    * If the page is an other page we use a longer cache TTL

--- wp-content/plugins/advisories-headers/spec/headers.spec.php
+++ wp-content/plugins/advisories-headers/spec/headers.spec.php
@@ -18,10 +18,56 @@ describe(\Dxw\AdvisoriesHeaders\Headers::class, function () {
 	});
 
 	describe('->addCacheControl()', function () {
-		it('does nothing', function () {
-			$headers = [];
-			$result = $this->headers->addCacheControl($headers);
-			expect($result)->toEqual($headers);
+		context('if the user is logged in', function () {
+			it('does not cache the page', function () {
+				allow('is_user_logged_in')->toBeCalled()->andReturn(true);
+				$expected = ['Cache-Control' => 'no-cache, private'];
+				$result = $this->headers->addCacheControl([]);
+				expect($result)->toEqual($expected);
+			});
+		});
+		context('if the user is not logged in and the request is a search query', function () {
+			it('caches the page with a short TTL', function () {
+				allow('is_user_logged_in')->toBeCalled()->andReturn(false);
+				allow('is_search')->toBeCalled()->andReturn(true);
+				allow('is_front_page')->toBeCalled()->andReturn(false);
+				allow('is_archive')->toBeCalled()->andReturn(true);
+				$expected = ['Cache-Control' => 'public, max-age=1800'];
+				$result = $this->headers->addCacheControl([]);
+				expect($result)->toEqual($expected);
+			});
+		});
+		context('if the user is not logged in and the page is the homepage', function () {
+			it('caches the page with a short TTL', function () {
+				allow('is_user_logged_in')->toBeCalled()->andReturn(false);
+				allow('is_search')->toBeCalled()->andReturn(false);
+				allow('is_front_page')->toBeCalled()->andReturn(true);
+				$expected = ['Cache-Control' => 'public, max-age=1800'];
+				$result = $this->headers->addCacheControl([]);
+				expect($result)->toEqual($expected);
+			});
+		});
+		context('if the user is not logged in and the page is an archive page', function () {
+			it('caches the page with a short TTL', function () {
+				allow('is_user_logged_in')->toBeCalled()->andReturn(false);
+				allow('is_search')->toBeCalled()->andReturn(false);
+				allow('is_front_page')->toBeCalled()->andReturn(false);
+				allow('is_archive')->toBeCalled()->andReturn(true);
+				$expected = ['Cache-Control' => 'public, max-age=1800'];
+				$result = $this->headers->addCacheControl([]);
+				expect($result)->toEqual($expected);
+			});
+		});
+		context('if the user is not logged in and the page is not the homepage or an archive page', function () {
+			it('does caches the page with a long TTL', function () {
+				allow('is_user_logged_in')->toBeCalled()->andReturn(false);
+				allow('is_search')->toBeCalled()->andReturn(false);
+				allow('is_front_page')->toBeCalled()->andReturn(false);
+				allow('is_archive')->toBeCalled()->andReturn(false);
+				$expected = ['Cache-Control' => 'public, max-age=86400'];
+				$result = $this->headers->addCacheControl([]);
+				expect($result)->toEqual($expected);
+			});
 		});
 	});
 });
--- wp-content/plugins/advisories-headers/src/Headers.php
+++ wp-content/plugins/advisories-headers/src/Headers.php
@@ -4,17 +4,32 @@ namespace Dxw\AdvisoriesHeaders;
 
 class Headers
 {
+	private int $ttl_short = 1800;  // 30 mins..
+	private int $ttl_long = 86400;  // 1 day.
+
 	public function register(): void
 	{
 		add_filter('wp_headers', [$this, 'addCacheControl']);
 	}
 
 	/**
+	 * Add Cache-Control headers to the site.
+	 *
+	 * If the user is logged in, we do not cache; if the page requested is an
+	 * archive or homepage we have a short TTL, otherwise we use a longer TTL.
+	 *
 	 * @param string[] $headers
 	 * @return string[]
 	 */
 	public function addCacheControl(array $headers): array
 	{
+		if (is_user_logged_in()) {
+			$headers['Cache-Control'] = 'no-cache, private';
+		} elseif (is_front_page() || is_archive() || is_search()) {
+			$headers['Cache-Control'] = 'public, max-age=' . $this->ttl_short;
+		} else {
+			$headers['Cache-Control'] = 'public, max-age=' . $this->ttl_long;
+		}
 		return $headers;
 	}
 }
