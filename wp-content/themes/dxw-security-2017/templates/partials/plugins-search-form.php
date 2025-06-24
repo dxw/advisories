7a83f7b2336155f67361069bbf540cd80be2ab28
Sarah Mount <sarah.mount@dxw.com>
2025-06-24 10:00:00 +0100

    Remove visible label on search forms
    And replace with an aria-label. This gives a cleaner
    look and avoids the awkward distance between the
    input element and the label.

--- wp-content/themes/dxw-security-2017/templates/partials/plugins-search-form.php
+++ wp-content/themes/dxw-security-2017/templates/partials/plugins-search-form.php
@@ -1,10 +1,9 @@
 <div class="search-form-container">
     <div class="row">
         <form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url('/') ?>">
-            <label for="s" class="block-label">Search plugin reviews:</label>
             <div class="form-field">
                 <input type="hidden" name="post_type" value="plugins">
-                <input type="search" value="<?php echo get_search_query() ?>" name="s" id="s">
+                <input type="search" aria-label="Search plugins" value="<?php echo get_search_query() ?>" name="s" id="s">
                 <button value="Search" class="button">Search</button>
             </div>
         </form>
