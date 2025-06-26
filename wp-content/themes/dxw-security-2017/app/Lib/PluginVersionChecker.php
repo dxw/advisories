dxw/advisories

commit 42f3a99b03827f5bb598616e8d416484018c2ebb

Fix creation of dynamic property warnings

wp-content/themes/dxw-security-2017/app/Lib/PluginVersionChecker.php
@@ -4,6 +4,13 @@ namespace Dxw\DxwSecurity2017\Lib;
 
 class PluginVersionChecker
 {
+	private int $id;
+	private array $versions;
+	private string $version;
+	private string $is_codex;
+	private string $codex_link;
+	private string $slug;
+
 	public function __construct()
 	{
 		$this->id = (int)get_the_ID();

commit ba7f390e75552c0c051ab15764c595bfda1f15db

FIX type error in PluginVersionChecker
Introduced in 42f3a99b03827f5bb598616e8d416484018c2ebb

wp-content/themes/dxw-security-2017/app/Lib/PluginVersionChecker.php
@@ -7,7 +7,7 @@ class PluginVersionChecker
 	private int $id;
 	private array $versions;
 	private string $version;
-	private string $is_codex;
+	private bool $is_codex;
 	private string $codex_link;
 	private string $slug;
 