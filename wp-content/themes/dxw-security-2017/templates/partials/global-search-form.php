ed933f3e53087e4cb25e99fc8a2d009546a281bb
Sarah Mount <sarah.mount@dxw.com>
2025-06-29 22:38:51 +0100

    FIX quoting in HTML for search form

--- wp-content/themes/dxw-security-2017/templates/partials/global-search-form.php
+++ wp-content/themes/dxw-security-2017/templates/partials/global-search-form.php
@@ -2,7 +2,7 @@
     <div class="row">
         <form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url('/') ?>">
             <div class="form-field">
-                <input type="search" aria-label="Search plugins and advisories value="<?php echo get_search_query() ?>" name="s" id="s">
+                <input type="search" aria-label="Search plugins and advisories" value="<?php echo get_search_query() ?>" name="s" id="s">
                 <button value="Search" class="button">Search</button>
             </div>
         </form>
