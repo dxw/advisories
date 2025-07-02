5bbfe09b578cbcbd19948866e4df3f6b50b536a6
Sarah Mount <sarah.mount@dxw.com>
2025-07-02 18:26:59 +0100

    Simplify pagination links
    This commit simplifies the links we have for pagination,
    firstly by removing assets (Javascript and images) that
    we ship with this site just for pagination.
    
    Secondly we use the_posts_pagination() now for creating
    the links themselves, rather than the old h() call.
    
    Lastly we simplify much of the styling, allowing most
    of the styling to fall back on the base styles for
    the site.

--- wp-content/themes/dxw-security-2017/assets/js/common.js
+++ wp-content/themes/dxw-security-2017/assets/js/common.js
@@ -2,14 +2,3 @@
 
 require('./components/StickyNavigation')
 require('./components/ProgressiveDisclosure')
-
-jQuery(function ($) {
-  'use strict'
-
-  // Change previous/next navigation in pager
- $(function () {
-   $('.arrow.previous a').html('<span class="hidden-min-width underline">Previous<span class="sr-only"> page</span></span>');
-   $('.arrow.next a').html('<span class="hidden-min-width underline">Next<span class="sr-only"> page</span></span>');
- })
-
-})
