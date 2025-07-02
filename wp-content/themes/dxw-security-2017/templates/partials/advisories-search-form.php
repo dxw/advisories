771f6ca9388e6d5b07785055afaa731af563d27c
Sarah Mount <sarah.mount@dxw.com>
2025-07-02 12:23:05 +0100

    Replace aria-label with hidden label element
    This commit amends 7a83f7b2336155f67361069bbf540cd80be2ab28
    by adding back the label element to the three search
    forms but making it only available to screen readers.

--- wp-content/themes/dxw-security-2017/templates/partials/advisories-search-form.php
+++ wp-content/themes/dxw-security-2017/templates/partials/advisories-search-form.php
@@ -2,8 +2,9 @@
     <div class="row">
         <form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url('/') ?>">
             <div class="form-field">
+                <label for="s" class="block-label sr-only">Search advisories:</label>
                 <input type="hidden" name="post_type" value="advisories">
-                <input type="search" aria-label="Search advisories" value="<?php echo get_search_query() ?>" name="s" id="s">
+                <input type="search" value="<?php echo get_search_query() ?>" name="s" id="s">
                 <button value="Search" class="button">Search advisories</button>
             </div>
         </form>
--- wp-content/themes/dxw-security-2017/templates/partials/global-search-form.php
+++ wp-content/themes/dxw-security-2017/templates/partials/global-search-form.php
@@ -2,6 +2,7 @@
     <div class="row">
         <form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url('/') ?>">
             <div class="form-field">
+                <label for="s" class="block-label sr-only">Search plugins &amp; advisories:</label>
                 <input type="search" aria-label="Search plugins and advisories" value="<?php echo get_search_query() ?>" name="s" id="s">
                 <button value="Search" class="button">Search</button>
             </div>
