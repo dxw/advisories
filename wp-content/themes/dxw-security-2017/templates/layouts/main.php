c00b7962834cc6924a3793b7af82c7468008df99
Sarah Mount <sarah.mount@dxw.com>
2025-07-03 22:36:21 +0100

    Add a meta description tag to each page
    For generic pages we just use the site title and tagline.
    
    For advisories and reviews we always state the page title
    with the status (for advisories) and a publishing date,
    to be clear about whether the information on this site
    is out of date.

--- wp-content/themes/dxw-security-2017/templates/layouts/main.php
+++ wp-content/themes/dxw-security-2017/templates/layouts/main.php
@@ -2,6 +2,15 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
     <meta charset="utf-8">
+    <meta name="description" content="<?php
+    if (get_post_type() === 'plugins') {
+        echo esc_attr('dxw plugin review: ' . single_post_title('', false) . ' - ' . get_the_date());
+    } elseif (get_post_type() === 'advisories') {
+        echo esc_attr('dxw advisory: ' . single_post_title('', false) . ' (' . esc_html(get_field('workflow_state')) . ')' . ' - ' . get_the_date());
+    } else {
+        echo esc_attr(get_bloginfo('name') . ' - ' . get_bloginfo('description'));
+    }
+    ?>" />
     <script defer data-domain="advisories.dxw.com" src="https://plausible.io/js/script.js"></script>
 
     <?php wp_head(); ?>
