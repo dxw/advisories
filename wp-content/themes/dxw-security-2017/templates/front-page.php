82a9231d2a31c1ad0e55cfa99c5283d269cafa4b
Sarah Mount <sarah.mount@dxw.com>
2025-07-04 09:06:56 +0100

    Simplify how archive lists are presented
    Previously we've had a number of a11y issues on this site
    due to headings being out of order. The way that archive
    lists are styled is also different on the front page
    and the two main archive pages and could be improved.
    
    This is partly due to the archive lists being generated
    in two different ways. On the archive pages we used
    a partial and on the home page we did not.
    
    This commit simplifies that a little, by using the
    partial everywhere. The partial now needs a switch
    to decide which heading level it should produce,
    but this does mean we only have one piece of HTML
    to style.

--- wp-content/themes/dxw-security-2017/templates/front-page.php
+++ wp-content/themes/dxw-security-2017/templates/front-page.php
@@ -30,31 +30,23 @@
     <div class="row">
 
         <section class="homepage-feed-container feed-container">
-            <h4>Recent plugin recommendations</h4>
+            <h2>Recent plugin recommendations</h2>
             <?php global $post;
                 $args = array('post_type' => 'Plugins');
                 $custom_posts = get_posts($args);
                 foreach($custom_posts as $post) : setup_postdata($post); ?>
-                <article class="short-review">
-                    <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
-                    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-                    <?php h()->the_short_recommendation(); ?>
-                </article>
+                <?php get_template_part('partials/article-list-item'); ?>
             <?php endforeach; ?>
             <a href="/plugins" class="button">All plugin recommendations</a>
         </section>
 
         <section class="homepage-feed-container feed-container">
-            <h4>Recent advisories</h4>
+            <h2>Recent advisories</h2>
             <?php global $post;
                 $args = array('post_type' => 'Advisories');
                 $custom_posts = get_posts($args);
                 foreach($custom_posts as $post) : setup_postdata($post); ?>
-                <article class="short-review">
-                    <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
-                    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-                    <p class="score <?php echo strtolower(h()->get_cvss_severity()); ?>">Severity: <span class="score"><?php h()->the_cvss_severity(); ?></span></p>
-                </article>
+                <?php get_template_part('partials/article-list-item'); ?>
             <?php endforeach; ?>
             <a href="/advisories" class="button">All advisories</a>
         </section>
